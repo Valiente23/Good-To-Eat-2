a02a4d36668e5a5e151e510ad395c3cc4013169d
Braian OIiva <73452780+Valiente23@users.noreply.github.com>
2021-08-14 11:05:23 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-
-function deslizarNav(){
-  let burgerMenu = document.querySelector(".menu__icon")
-  if(burgerMenu){
-    burgerMenu.addEventListener('click', ()=>{
-      let navResponsive = document.querySelector(".nav__container")
-      navResponsive.classList.toggle('show-nav')
-    })
+function deslizarNav() {
+  let burgerMenu = document.querySelector(".menu__icon");
+  if (burgerMenu) {
+    burgerMenu.addEventListener("click", () => {
+      let navResponsive = document.querySelector(".nav__container");
+      navResponsive.classList.toggle("show-nav");
+    });
   }
 }
-deslizarNav();
+function deslizarRecetas(id) {
+    let receta = document.querySelectorAll(".card__item")[id];
+    receta.addEventListener("click", () => {
+      let receta = document.querySelectorAll(".card__item")[id];
+      receta.classList.toggle("show-instructions");
+      let instrucciones = document.querySelectorAll(`.card__instructions`)[id];
+      instrucciones.classList.toggle("show-recetas");
+    });
+}
 
+let recetas = document.querySelectorAll(".card__item")
+for (let i = 0; i < recetas.length; i++) {
+  deslizarRecetas(i);
+}
+
+deslizarNav();
